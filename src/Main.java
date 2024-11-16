467f1ddc88fde19370029b7b3cab3e0112eee4b7
MartinPineda-18 <jmpinedarubiano@gmail.com>
2024-11-15 23:41:49 -0500

    Menu interactivo usuario

--- src/Main.java
+++ src/Main.java
@@ -5,40 +5,114 @@ public class Main {
 
         Gimnasio gimnasio = new Gimnasio();
         Scanner teclado = new Scanner(System.in);
+        Cliente cliente = new Cliente();
+        Entrenador entrenador = new Entrenador();
 
-        Cliente cliente1 = new Cliente("Juan", 18, "1019604513","Premium");
-        Cliente cliente2 = new Cliente("Maria", 20,"30908765","Premium");
-        Entrenador entrenador1 = new Entrenador("Julian", 30, "2089764127","Tren superior");
+        int opc;
 
-        gimnasio.agregarPersona(cliente1);
-        gimnasio.agregarPersona(cliente2);
-        gimnasio.agregarPersona(entrenador1);
-        gimnasio.mostrarPersonas();
-        gimnasio.eliminarPersona("30908765");
-
-        System.out.println("\nBuscando cliente con cedula 1019604513:");
-        Persona personaBuscada = gimnasio.buscarPersona("1019604513");
-        if (personaBuscada != null) {
-            System.out.println(personaBuscada.obtenerInformacion());
-        } else {
-            System.out.println("Cliente no encontrado.");
-        }
-
-        /*int opc;
+        do {
 
         System.out.println("Ingrese la opcion deseada: \n" +
                 "1) Agregar persona\n" +
                 "2) Eliminar persona\n" +
                 "3) Mostrar personas\n" +
-                "4) Buscar persona\n");
+                "4) Buscar persona\n" +
+                "5) Salir");
         opc = teclado.nextInt();
+        teclado.nextLine();
 
         switch (opc) {
 
             case 1:
-                System.out.println("Ingrese el nombre de la persona: ");
-                teclado.next(new Cliente())
-        }*/
+                System.out.println("Ingrese la opcion deseada: \n" +
+                        "1) Cliente\n" +
+                        "2) Entrenador\n" +
+                        "3) Salir\n");
+                int opc1 = teclado.nextInt();
+                teclado.nextLine();
 
+                if (opc1 == 1){
+                    System.out.println("Datos cliente: \n");
+
+                    System.out.println("Ingrese nombre: ");
+                    cliente.setNombre(teclado.nextLine());
+
+                    System.out.println("Ingrese edad: ");
+                    cliente.setEdad(teclado.nextInt());
+                    teclado.nextLine();
+
+                    System.out.println("Ingrese cedula: ");
+                    cliente.setId(teclado.nextLine());
+
+                    System.out.println("Ingrese el tipo de membresia: ");
+                    cliente.setTipoMembresia(teclado.nextLine());
+
+                    gimnasio.agregarPersona(cliente);
+
+                } else if (opc1 == 2) {
+
+                    System.out.println("Datos entrenador: \n");
+
+                    System.out.println("Ingrese nombre: ");
+                    entrenador.setNombre(teclado.nextLine());
+
+                    System.out.println("Ingrese edad: ");
+                    entrenador.setEdad(teclado.nextInt());
+                    teclado.nextLine();
+
+                    System.out.println("Ingrese cedula: ");
+                    entrenador.setId(teclado.nextLine());
+
+                    System.out.println("Ingrese la especialidad del entrenador: ");
+                    entrenador.setEspecialidad(teclado.nextLine());
+
+                    gimnasio.agregarPersona(entrenador);
+
+                } else if (opc1 == 3){
+
+                    System.out.println("Volviendo al menu principal...");
+
+                } else {
+
+                    System.out.println("Opcion no valida");
+
+                }
+                break;
+
+            case 2:
+
+                System.out.print("Ingrese la cedula de la persona a eliminar: ");
+                String idEliminar = teclado.nextLine();
+                gimnasio.eliminarPersona(idEliminar);
+                break;
+
+            case 3:
+
+                gimnasio.mostrarPersonas();
+                break;
+
+            case 4:
+
+                System.out.print("Ingrese la cedula de la persona a buscar: ");
+                String idBuscar = teclado.nextLine();
+                Persona persona = gimnasio.buscarPersona(idBuscar);
+                if (persona != null) {
+                    System.out.println(persona.obtenerInformacion());
+                } else {
+                    System.out.println("Cedula no registrada");
+                }
+                break;
+
+            case 5:
+
+                System.out.println("Saliendo del menu");
+                break;
+
+            default:
+
+                System.out.println("Opción no válida");
+
+            }
+        } while (opc != 5);
     }
 }
